3b3f19fdb52c6011fc4d2c6828c2eb75c7bb1671
karolisMtb <karolis.mtb@gmail.com>
2023-04-13 15:54:03 +0300

    Add project files.

---
+++ QuoterApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace QuoterApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var gq = new YourQuoter();
+            var qty = 120;
+
+            double quote = 0;
+            double vwap = 0;
+
+            quote = gq.GetQuote("DK50782120", qty);
+            vwap = gq.GetVolumeWeightedAveragePrice("DK50782120");
+
+            Console.WriteLine($"Quote: {quote}, {quote / (double)qty}");
+            Console.WriteLine($"Average Price: {vwap}");
+            Console.WriteLine();
+            Console.WriteLine($"Done");
+        }
+    }
+}
---
+++ QuoterApp/YourQuoter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QuoterApp
+{
+    public class YourQuoter : IQuoter
+    {
+        public double GetQuote(string instrumentId, int quantity)
+        {
+            HardcodedMarketOrderSource source = new HardcodedMarketOrderSource();
+            List<MarketOrder> orders = new List<MarketOrder>();
+
+            Thread fetchThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    MarketOrder marketOrder = source.GetNextMarketOrder();
+                    if (marketOrder.InstrumentId == instrumentId)
+                    {
+                        orders.Add(marketOrder);
+                    }
+                }
+            });
+
+            double quote = 0;
+
+            Thread serviceThread = new Thread(() =>
+            {
+                quote = orders.OrderByDescending(x => x.Price).Last().Price;
+
+            });
+
+            fetchThread.Start();
+            fetchThread.Join();
+            serviceThread.Start();
+            serviceThread.Join();
+
+            return quote;
+        }
+
+        public double GetVolumeWeightedAveragePrice(string instrumentId)
+        {
+            HardcodedMarketOrderSource source = new HardcodedMarketOrderSource();
+            List<MarketOrder> orders = new List<MarketOrder>();
+
+            double temp = 0;
+            int totalQuantity = 0;
+            double vwap = 0;
+
+            Thread fetchThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    MarketOrder marketOrder = source.GetNextMarketOrder();
+                    if (marketOrder.InstrumentId == instrumentId)
+                    {
+                        orders.Add(marketOrder);
+                    }
+                }
+            });
+
+
+            Thread vwapServiceThread = new Thread(() =>
+            {
+                if (orders.Count != 0)
+                {
+                    foreach (MarketOrder order in orders)
+                    {
+                        temp += order.Price * order.Quantity;
+                        totalQuantity += order.Quantity;
+                    }
+
+                    vwap = totalQuantity / totalQuantity;
+                }
+            });
+
+            fetchThread.Start();
+            vwapServiceThread.Start();
+            vwapServiceThread.Join();
+
+            return vwap;
+        }
+    }
+}
